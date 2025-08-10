46d967b8d0e3b7d57783e440cb4e0060ad5821e8
ashesh808 <37954173+ashesh808@users.noreply.github.com>
2025-08-09 20:28:57 -0500

    del: Removing msc files

--- views/login.py
+++
@@ -1,8 +0,0 @@
-from z8ter.page import Page
-from starlette.requests import Request
-from starlette.responses import Response
-
-
-class Login(Page):
-    async def get(self, request: Request) -> Response:
-        return self.render(request, "login.jinja", {})
